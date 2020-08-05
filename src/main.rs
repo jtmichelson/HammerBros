c918a52f9e8156c4a80a4f95a4c842f04f1bfd3d
jtmichelson <jasontmichelson@gmail.com>
2020-08-05 16:59:51 -0700

    Add player type with location information.

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,54 @@
-use ggez::{graphics, mint, Context, ContextBuilder, GameResult};
+mod player;
+
 use ggez::conf::{WindowMode, WindowSetup};
-use ggez::event::{self, EventHandler};
+use ggez::event::{self, EventHandler, EventsLoop};
 use ggez::graphics::Color;
+use ggez::{graphics, Context, ContextBuilder, GameResult};
+
+use crate::player::Player;
+
+const GREY: Color = Color::new(0.50, 0.50, 0.50, 1.00);
+const TURQUOISE: Color = Color::new(0.00, 1.00, 1.00, 1.00);
+const ORANGE: Color = Color::new(1.00, 0.40, 0.10, 1.00);
 
 fn main() {
-    // Make a Context.
-    let window_mode: WindowMode = WindowMode::default()
-        .dimensions(1800.0, 1000.0); // 1080p with some padding
+    // 1080p window with some margin.
+    let window_mode: WindowMode = WindowMode::default().dimensions(1800.0, 1000.0);
 
-    let window_setup: WindowSetup = WindowSetup::default()
-        .title("Hammer Bros");
+    let window_setup: WindowSetup = WindowSetup::default().title("Hammer Bros");
 
-    let (mut context, mut event_loop) = ContextBuilder::new("hammer_bros", "This Boiz Games")
-        .window_mode(window_mode)
-        .window_setup(window_setup)
-        .build()
-        .expect("aieee, could not create ggez context!");
+    let (mut context, mut event_loop): (Context, EventsLoop) =
+        ContextBuilder::new("hammer_bros", "We Dem Boiz Games")
+            .window_mode(window_mode)
+            .window_setup(window_setup)
+            .build()
+            .expect("Hã!? Could not create ggez context!");
 
     // Create an instance of your event handler.
     // Usually, you should provide it with the Context object to
     // use when setting your game up.
-    let mut my_game = MyGame::new(&mut context);
+    let mut my_game: MyGame = MyGame::new(&mut context).unwrap();
 
     // Run!
     match event::run(&mut context, &mut event_loop, &mut my_game) {
         Ok(_) => println!("Exited cleanly."),
-        Err(e) => println!("Error occured: {}", e)
+        Err(e) => println!("Error occured: {}", e),
     }
 }
 
 pub struct MyGame {
     // Your state here...
+    players: Vec<Player>,
 }
 
 impl MyGame {
-    pub fn new(_context: &mut Context) -> MyGame {
+    pub fn new(context: &mut Context) -> GameResult<MyGame> {
         // Load/create resources such as images here.
-        MyGame {
-            // ...
-        }
+        let player1: Player = Player::new(200.0, 200.0, context, TURQUOISE)?;
+        let player2: Player = Player::new(800.0, 800.0, context, ORANGE)?;
+        let players: Vec<Player> = vec![player1, player2];
+
+        Ok(MyGame { players })
     }
 }
 
@@ -49,20 +59,11 @@ impl EventHandler for MyGame {
     }
 
     fn draw(&mut self, context: &mut Context) -> GameResult<()> {
-        let grey: Color = [0.5, 0.5, 0.5, 1.0].into();
-        let turquoise: Color = [0.00, 0.42, 0.5, 1.0].into();
+        graphics::clear(context, GREY);
 
-        graphics::clear(context, grey);
-        // Draw code here...
-        let circle = graphics::Mesh::new_circle(
-            context,
-            graphics::DrawMode::fill(),
-            mint::Point2{x: 400.0, y: 500.0},
-            200.0,
-            0.1,
-            turquoise,
-        )?;
-        graphics::draw(context, &circle, graphics::DrawParam::default())?;
+        for i in 0..self.players.len() {
+            self.players[i].draw(context)?;
+        }
 
         graphics::present(context)?;
         Ok(())
